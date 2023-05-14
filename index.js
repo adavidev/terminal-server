58d0c371a18a16513e217fc004621b687e1cc27f
Alan Davis <adavis@MacBook-Pro-3.local>
2023-05-14 13:16:46 -0600

    prep

---
+++ index.js
@@ -0,0 +1,15 @@
+// index.js
+
+const express = require('express');
+const app = express();
+
+// Use port from Heroku, fall back to default port
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`App available at http://localhost:${port}`);
+});
