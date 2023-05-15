2d52a2f6c22ea09f8f5d6a098b8e25877ab1f2f8
Alan Davis <adavis@MacBook-Pro-3.local>
2023-05-14 20:38:41 -0600

    db fix

--- db.js
+++ db.js
@@ -2,7 +2,13 @@ const { Sequelize } = require('sequelize');
 console.log(process.env.DATABASE_URL)
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'postgres'
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false // <<<<<<< YOU NEED THIS
+    }
+  },
 });
 
 module.exports = sequelize;
