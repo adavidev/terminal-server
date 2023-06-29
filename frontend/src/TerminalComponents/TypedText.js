42a7dda649c91ff18906ceff8fc78bcce24af204
Alan Davis <adavis@MacBook-Pro-3.local>
2023-06-29 11:21:24 -0600

    attempting fix for text memory leak

--- frontend/src/TerminalComponents/TypedText.js
+++ frontend/src/TerminalComponents/TypedText.js
@@ -5,24 +5,26 @@ const Typewriter = ({ text, doneCallback = () => {} }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
   useEffect(() => {
+    let timeout;
+
     if (currentIndex < text.length) {
-      const timeout = setTimeout(() => {
+      timeout = setTimeout(() => {
         setTypedText(prevText => prevText + text[currentIndex]);
         setCurrentIndex(prevIndex => prevIndex + 1);
       }, 50);
-  
-      return () => clearTimeout(timeout);
     } else {
-      setTimeout(() => {
+      timeout = setTimeout(() => {
         doneCallback();
       }, 200);
     }
+
+    return () => clearTimeout(timeout);
   }, [currentIndex, text]);
 
   useEffect(() => {
-    setTypedText('')
-    setCurrentIndex(0)
-  }, [text])
+    setTypedText('');
+    setCurrentIndex(0);
+  }, [text]);
 
   return <span>{typedText}</span>;
 };
